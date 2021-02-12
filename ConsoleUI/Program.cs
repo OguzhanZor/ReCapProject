1af53ba462e596db747fbe3cb7947d99a7784848
Oguzhan Zor <hanoguzzor@gmail.com>
2021-02-12 20:35:01 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carService = new CarManager(new InMemoryCarDal());
+            GetAllMethod(carService);
+            GetByIdMethot(carService);
+
+          
+
+        }
+        static void GetAllMethod(ICarService carService)
+        {
+            foreach (var item in carService.GetAll())
+            {
+                Console.WriteLine(item.Id + " " + item.ModelYear + " " + item.BrandId + " " + item.ColorId + " " + item.DailyPrice + " " + item.Description);
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        static void GetByIdMethot(ICarService carService)
+        {
+            var car = carService.GetById(2);
+            Console.WriteLine(car.Id + " " + car.ModelYear + " " + car.Description);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car {Id=1,BrandId=1,ColorId=15,ModelYear="1995",DailyPrice=150,Description="Acıklama"},
+                new Car {Id=2,BrandId=2,ColorId=68,ModelYear="1996",DailyPrice=12,Description="Acıklama"},
+                new Car {Id=3,BrandId=2,ColorId=25,ModelYear="1997",DailyPrice=1852,Description="Acıklama"},
+                new Car {Id=4,BrandId=1,ColorId=17,ModelYear="1998",DailyPrice=1954,Description="Acıklama"},
+                new Car {Id=5,BrandId=2,ColorId=12,ModelYear="1999",DailyPrice=160,Description="Acıklama"}
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p => p.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            Car carToGet = _cars.SingleOrDefault(p => p.Id == id);
+            return carToGet;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+
+        }
+
+
+    }
+}
